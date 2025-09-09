702a97bd6419c1d9bc9b104d4c37899a00f3d570
Rahul <rahul.mymail1@gmail.com>
2025-09-09 05:36:44 +0530

    init trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
-import { Button } from "@/components/ui/button";
+// "use client";
+// import { useTRPC } from "@/trpc/client";
+// import { useQuery } from "@tanstack/react-query";
+
+// export default function Home() {
+
+//   const trpc = useTRPC();
+//   const {data} = useQuery(trpc.hello.queryOptions({ text: 1 }));
+//   console.log(data);
+
+//   return (
+//     <div className="text-3xl font-bold underline">
+//       {JSON.stringify(data)}
+//     </div>
+//   );
+// }
+ 
+
+//----------------------------------------------------------------------------------
+import { caller } from "@/trpc/server";
 
 export default async function Home() {
+  const data = await caller.hello({ text: 1 });
   return (
     <div className="text-3xl font-bold underline">
-      <Button variant="destructive" className="cursor-pointer">Click me 2</Button>
+      {JSON.stringify(data)}
     </div>
   );
 }
- 
